de3750a83f40e8076563ee84663b7afee3e61a4a
Amadou Diop <diop.amadou8@gmail.com>
2020-06-01 23:46:59 +0000

    Correction de la taille de la tof dans le logo

--- application/views/nav_admin.php
+++ application/views/nav_admin.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="<?php echo base_url() ?>">
-            <img class="logo_img" src="<?php echo base_url(); ?>assets/img/logo_trns2.png" alt="">
+            <img class="logo_img" src="<?php echo base_url(); ?>assets/img/album/logo_gauche.png" alt="">
             SenMask221
-            <img class="logo_img" src="<?php echo base_url(); ?>assets/img/logo_trns.png" alt="">
+            <img class="logo_img" src="<?php echo base_url(); ?>assets/img/album/logo_droit.png" alt="">
         </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
